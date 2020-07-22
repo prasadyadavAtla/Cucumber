0578c9272d1080e1655fabd255708cf08ac60b3e
venkataprasad <prasadbabu457@gmail.com>
2020-07-22 12:53:36 +0530

    commiting new file

--- src/test/java/CucumberOption.java
+++ src/test/java/CucumberOption.java
@@ -4,7 +4,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/feature/login.feature",glue="stepdefinition",tags="@SanityTest")
+@CucumberOptions(features="src/test/java/feature/login.feature",glue="stepdefinition",tags="@MobileTest",plugin={"pretty", "html:target/cucumber","json:target/cucumber.json","junit:/taget/cukes.xml"})
 public class CucumberOption {
 
 }
